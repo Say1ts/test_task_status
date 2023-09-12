9d6eb4d08f19022111d265f1b5db345176d96cf6
say1ts <sayan.tsydenov@expertonica.com>
2023-09-12 15:48:54 +0800

    Add empty class and structure

---
+++ src/TreeStore.py
@@ -0,0 +1,15 @@
+class TreeStore:
+    def __init__(self, items: list[dict]):
+        pass
+
+    def getAll(self) -> list[dict]:
+        pass
+
+    def getItem(self, id: int) -> dict:
+        pass
+
+    def getChildren(self, id: int) -> list[dict]:
+        pass
+
+    def getAllParents(self):
+        pass
